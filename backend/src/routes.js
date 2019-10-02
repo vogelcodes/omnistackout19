ebe3091dda6b0e4fd7cac761757ca5a1f0d164f2
Daniel <danielcvt@gmail.com>
2019-10-02 16:34:30 -0300

    spot/model&controller OK

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,15 @@
-//la la la
 const express = require('express');
-const routes = express.Router();
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
 const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+
+const routes = express.Router();
+const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
+routes.post('/spots',upload.single('thumbnail'), SpotController.store);
+
 
 module.exports = routes;
