2e937110d69cf00f4b34a244cda3ac043e98a47a
Daniel <danielcvt@gmail.com>
2019-10-02 17:22:35 -0300

    dashboard

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -2,10 +2,16 @@ const Spot = require('../models/Spot');
 const User = require('../models/User');
 
 module.exports = {
+    async index(req, res){
+        const { tech } = req.query;
+        const spots = await Spot.find({ techs: tech });
+        return res.json(spots);
+
+    },
     async store(req, res) {
         const { filename } = req.file;
         const { company, techs, price } = req.body;
-        const {user_id} = req.headers;
+        const { user_id } = req.headers;
 
         const user = await User.findById(user_id);
 
