c14c00fa69ba8d56e3f517940243a6ba2211d2fe
Daniel <danielcvt@gmail.com>
2019-10-01 21:44:52 -0300

    session controller require bug

---
+++ backend/controllers/SessionController.js
@@ -0,0 +1,6 @@
+//index show store update destroy
+module.exports = {
+    store(req,res) {
+        return res.JSON({ message: 'Hello World'})
+    }
+};
---
+++ backend/src/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const routes = express.Router();
+
+routes.post('/users', (req, res) => {
+    return res.json(req.body);
+});
+
+module.exports = routes;
