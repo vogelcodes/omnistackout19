bb5b491e6f5e59908b2a0c538c61887fc9874036
Daniel <danielcvt@gmail.com>
2019-10-04 14:39:46 -0300

    dashboars spot-list

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 const app = express();
@@ -12,6 +13,9 @@ mongoose.connect('mongodb+srv://vogelcodes:vogelcodes18@vogelcodes-kpzli.mongodb
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
+
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import api from './services/api';
 
-import logo from './assets/logo.svg'
+
+import logo from './assets/logo.svg';
+import Routes from './routes';
+
+
 
 function App() {
-  const [email, setEmail] = useState('');
-
-  async function handleSubmit(event){
-    event.preventDefault();
-    
-    const response = await api.post('/sessions', { email });
-    const {_id} = response.data;
-    localStorage.setItem('user', _id);
-    console.log(_id);
-    
-  }
+ 
   return (
     <div className="container">
       <img src={logo} alt="aircnclogo" srcSet=""/>
       <div className="content">
-        <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para seu time</p>  
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">E-mail</label>
-        <input
-          id="email"
-          type="email"
-          placeholder="Seu melhor e-mail"
-          value={email}
-          onChange={event => setEmail(event.target.value)}
-        />
-        <button className="btn" type="submit">Entrar</button>
-
-      </form>
+        <Routes />
       </div>
       
       
